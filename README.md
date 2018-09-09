ff367302f47acb1b4411f81d6709da45b37ccd6f
Lee Olayvar <leeola@fastmail.com>
2018-09-09 08:43:18 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# gRPCrw (unstable)
+
+gRPCrw is a small Go package that implements Go IO helpers for use with gRPC.
+
+This package will be unstable until the dev experience is fleshed out a bit more.
---
+++ grpcrw.go
@@ -0,0 +1,63 @@
+package grpcrw
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Recver interface {
+	RecvBytes() ([]byte, error)
+}
+
+type Sender interface {
+	SendBytes() ([]byte, error)
+}
+
+type RecverFunc func() ([]byte, error)
+
+type Reader struct {
+	Recver
+	buf bytes.Buffer
+}
+
+func NewReader(r Recver) Reader {
+	return Reader{
+		Recver: r,
+	}
+}
+
+func NewReaderF(f func() ([]byte, error)) Reader {
+	return NewReader(RecverFunc(f))
+}
+
+func (r Reader) Read(p []byte) (int, error) {
+	if r.Recver != nil {
+		b, err := r.RecvBytes()
+		if err != nil {
+			return 0, fmt.Errorf("recvbytes: %v", err)
+		}
+
+		// TODO(leeola): determine how we'll implicitly determine EOF.
+		// 0 bytes seems a decent starting point. Though, we may want to
+		// support 0, since Go readers do.
+		isEOF := len(b) == 0
+
+		if !isEOF {
+			// write recv'd bytes to buf, as it may be more than we can read into
+			// p
+			if _, err := r.buf.Write(b); err != nil {
+				return 0, fmt.Errorf("buf write: %v", err)
+			}
+		} else {
+			// "close" this Recver so the Read will finish due to the Recver being EOF
+			r.Recver = nil
+		}
+	}
+
+	// read whatever length requested into p from the buf
+	return r.buf.Read(p)
+}
+
+func (f RecverFunc) RecvBytes() ([]byte, error) {
+	return f()
+}
